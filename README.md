e730a0f425b16935b5287b4007c47ad7ee9cf38f
sebastientromp <sebastien.tromp@gmail.com>
2023-01-16 13:08:20 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+A Hearthstone mod for Firestone that removes the "ghost" overlay when examining cards you don't own in your collection, thus letting you see all the card details.
+
+# Installation
+
+Just drop [the latest DLL](https://github.com/sebastientromp/hs-melon-remove-ghost-overlay/releases/latest/download/RemoveGhostOverlayWhenExaminingCard.dll) into your Hearthstone Mods folder (you need to have installed MelonLoader first).
+
+See https://github.com/Zero-to-Heroes/firestone/wiki/Mods for more Hearthstone mods, and some infos about how to install them.
---
+++ RemoveGhostOverlayWhenExaminingCardMod.cs
@@ -0,0 +1,40 @@
+﻿using MelonLoader;
+using HarmonyLib;
+using System.Linq;
+using System;
+using UnityEngine;
+
+namespace RemoveGhostOverlayWhenExaminingCard
+{
+    public class RemoveGhostOverlayWhenExaminingCardMod : MelonMod
+    {
+        public static MelonLogger.Instance SharedLogger;
+
+        public static string currentBobSkin = null;
+
+        public override void OnInitializeMelon()
+        {
+            RemoveGhostOverlayWhenExaminingCardMod.SharedLogger = LoggerInstance;
+            var harmony = this.HarmonyInstance;
+            harmony.PatchAll(typeof(LoadGhostActorIfNecessaryPatcher));
+        }
+    } 
+
+    public static class LoadGhostActorIfNecessaryPatcher
+    {
+        [HarmonyPatch(typeof(CraftingManager), "GetAndPositionNewActor")]
+        [HarmonyPrefix]
+        public static bool GetAndPositionNewActorPrefix(CraftingManager __instance, Actor oldActor, int numCopies, ref Actor __result)
+        {
+            RemoveGhostOverlayWhenExaminingCardMod.SharedLogger.Msg($"Showing non-ghost version of the card");
+            var nonGhostActorMethod = AccessTools.Method(typeof(CraftingManager), "GetNonGhostActor");
+            Actor actor = nonGhostActorMethod.Invoke(__instance, new object[] { oldActor }) as Actor;
+            if (actor != null)
+            {
+                actor.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+            }
+            __result = actor;
+            return false;
+        }
+    }
+}
